8310e110b92159b5364e9ef22aecba07f4f044b3
zzedddd <melegend.forever@protonmail.com>
2023-01-05 23:22:21 +0530

    added: blog page components

---
+++ src/lib/pageElements/header.svelte
@@ -0,0 +1,14 @@
+<script>
+    export let brand = "ZED.CODES"
+    export let quote = "Welcome to my Blog"
+</script>
+
+<div class="w-full m-0 p-0 bg-cover bg-bottom" style="background-image:url('cover.jpg'); height: 60vh; max-height:460px;">
+    <div class="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+        <!--Title-->
+            <p class="text-white font-extrabold text-3xl md:text-5xl">
+               { brand }
+            </p>
+            <p class="text-xl md:text-2xl text-gray-500"> { quote } </p>
+    </div>
+</div>
